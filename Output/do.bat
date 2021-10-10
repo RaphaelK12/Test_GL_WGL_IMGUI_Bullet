316e56ca8a1dd4678238bf8a918543d114d2a02c
RaphaelK12 <umqualquer78@gmail.com>
2021-10-10 14:49:10 -0300

    Remove unnecessary binaries.

--- Output/do.bat
+++
@@ -1,4 +0,0 @@
-"C:\Users\thiar\OneDrive\Documents\GitHub\TRK\Imagem004\Output\Win32_Release\Editor004.exe"
-"C:\Users\thiar\OneDrive\Documents\GitHub\TRK\Imagem004\Output\Win32_Release_DLL\Editor004.exe"
-"C:\Users\thiar\OneDrive\Documents\GitHub\TRK\Imagem004\Output\x64_Release\Editor004.exe"
-"C:\Users\thiar\OneDrive\Documents\GitHub\TRK\Imagem004\Output\x64_Release_DLL\Editor004.exe"
